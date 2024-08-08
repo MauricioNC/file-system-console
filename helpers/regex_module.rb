9233ca0f81eacd23fdd418404848543e154949c3
MauricioNC <mauricionc19@gmail.com>
2024-08-08 11:00:12 -0600

    feat: Updated logic to filter by year

--- helpers/regex_module.rb
+++ helpers/regex_module.rb
@@ -16,17 +16,7 @@ module RegexModule
   def filter_by_year(file_name = "", year = "2024")
     return nil if file_name.empty? || file_name.nil?
 
-    year_matched = nil
-
-    case year
-    when "2024"
-      year_matched = file_name.match(/([\d]{2}[\-\s_][\d]{2}[\-\s_](2024|24))/)
-    when "2023"
-      year_matched = file_name.match(/([\d]{2}[\-\s_][\d]{2}[\-\s_](2023|23))/)
-    when "2022"
-      year_matched = file_name.match(/([\d]{2}[\-\s_][\d]{2}[\-\s_](2022|22))/)
-    end
-
-    year_matched.nil? ? "" : year_matched[0]
+    date = date(file_name, year)
+    date.include?(year)
   end
 end
